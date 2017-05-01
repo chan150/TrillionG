595ef2b5c463977fe414420d389dfa2f816babde
Himchan Park <chan150@dgist.ac.kr>
2017-05-01 16:00:53 +0900

    Appending shell files

---
+++ run.sh
@@ -0,0 +1,7 @@
+
+# Set your environment
+MASTER=`hostname -f` 
+SPARK_HOME=${SPARK_HOME} 
+HDFS_HOME=`hadoop fs -df | grep hdfs | awk '{print $1}'`/user/$USER/
+
+$SPARK_HOME/bin/spark-submit --master spark://$MASTER:7077 --class kr.acon.TrillionG --jars lib/fastutil-7.0.12.jar,lib/dsiutils-2.3.3.jar TrillionG.jar $@ -hdfs $HDFS_HOME
