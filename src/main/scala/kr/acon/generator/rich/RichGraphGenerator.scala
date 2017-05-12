820b686d08cde77a7c92c14e9f7c7317110e3dc8
Himchan Park <chan150@dgist.ac.kr>
2017-05-13 01:29:24 +0900

    remove rich generator
    remove rich generator
    (it will be added later version)

--- src/main/scala/kr/acon/generator/rich/RichGraphGenerator.scala
+++
@@ -1,8 +0,0 @@
-package kr.acon.generator.rich;
-
-import kr.acon.generator.BaseGenerator
-
-class RichGraphGenerator extends BaseGenerator {
-  override val appName = "Rich graph generation"
-  
-}
