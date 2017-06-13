9a48d925783eff176778c3ebac7c3c4f1b706236
Himchan Park <chan150@dgist.ac.kr>
2017-06-13 15:31:09 +0900

    fix redundant logging issues

--- compile-local.sh
+++ compile-local.sh
@@ -2,3 +2,4 @@ bash compile.sh
 wget https://archive.apache.org/dist/spark/spark-2.1.0/spark-2.1.0-bin-hadoop2.7.tgz
 tar xzf spark-2.1.0-bin-hadoop2.7.tgz
 mv spark-2.1.0-bin-hadoop2.7 spark-2.1.0
+cp conf/log4j.properties spark-2.1.0/conf
